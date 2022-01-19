60b3213406b559e53dc8c2c668f0528b1e034758
dante2294 <97360732+dante2294@users.noreply.github.com>
2022-01-18 21:23:29 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,43 @@
+
+let initialBill = Number(prompt("What is your bill?"));
+let tax = (0.07*initialBill)+initialBill;
+let tip = 0.05*tax;
+let totalBill = tax+tip;
+
+
+
+
+
+
+document.body.innerHTML=totalBill;
+
+
+
+
+
+
+
+
+
+
+
+// Variables store information
+//let prompter = prompt("What is your name?")
+// static variables
+// let name =  "Dante";
+// let lname = "Lewis";
+
+//let lName = prompt("What is your last name?");
+
+//let fName = name+" "+lName;
+
+//let number1 = Number(prompt("First Number"));
+//let number2 = Number(prompt("Second Number"));
+//let sum = number1+number2;
+//let percy = sum*0.3;
+//let totalPrice = sum-percy;
+
+//document.body.innerHTML=prompter;
+//document.body.innerHTML=fName;
+//document.body.innerHTML=prompter;
+//document.body.innerHTML=totalPrice;
